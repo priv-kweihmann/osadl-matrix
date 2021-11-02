82b7f1b16d6c3f9b0e85bae48722517581a3cc95
Konrad Weihmann <kweihmann@outlook.com>
2021-11-02 09:36:59 +0100

    MANIFEST.in: add csv data
    and fully rely on MANIFEST instead of package_data and friends
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- setup.py
+++ setup.py
@@ -27,12 +27,6 @@ setuptools.setup(
     long_description_content_type = 'text/markdown',
     license_files = ('LICENSE.ccby40', 'LICENSE.Unlicensed'),
     packages = ['osadl_matrix'],
-    package_data = {
-        'osadl_matrix': ['*.csv'],
-    },
-    data_files = [
-        ('osadl_matrix', ['osadl-matrix.csv'])
-    ],
     install_requires = requirements,
     extras_require = {
         'dev': requirements_dev,
