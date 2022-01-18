33d740b5ebf0a208cc4027921c198b99d6acf13b
Konrad Weihmann <kweihmann@outlook.com>
2022-01-18 08:26:56 +0100

    scrapper: rewrite to new data
    upstream changed the interface a supplies us now with a json format.
    use that and store it as raw json + plus convert it to the previously
    used csv format.
    Remove the need for beautifulsoap as we don't need any html parsing
    anymore
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- scripts/scrapper.py
+++ scripts/scrapper.py
@@ -3,28 +3,39 @@
 # SPDX-License-Identifier: Unlicensed
 
 import csv
+import json
 import os
 
 import urllib3
-from bs4 import BeautifulSoup
 
-_URL = 'https://www.osadl.org/fileadmin/checklists/matrix.html'
-_DUMP = os.path.join(os.path.dirname(__file__), '..', 'osadl_matrix', 'osadl-matrix.csv')
+_URL = 'https://www.osadl.org/fileadmin/checklists/matrix.json'
+_DUMP_CSV = os.path.join(os.path.dirname(__file__), '..', 'osadl_matrix', 'osadl-matrix.csv')
+_DUMP_JSON = os.path.join(os.path.dirname(__file__), '..', 'osadl_matrix', 'osadl-matrix.json')
 
 
 def _get_document():
     http = urllib3.PoolManager()
     resp = http.request('GET', _URL)
-    return BeautifulSoup(resp.data.decode('utf-8'), 'html.parser')
+    return json.loads(resp.data.decode('utf-8'))
 
-def _create_matrix(html):
-    _csv = []
-    table = html.find('table', {'id': 'compat-matrix'})
-    for row in table.find_all('tr'):
-        _csv.append([(''.join(col.find_all(text=True))).encode('ascii', 'ignore').decode('utf-8') for col in row.find_all('td')])
+def _create_matrix(raw):
+    # dump to json
+    with open(_DUMP_JSON, 'w') as jsonfile:
+        json.dump(raw, jsonfile, sort_keys=True)
 
+    # create a csv
 
-    with open(_DUMP, 'w') as csvfile:
+    _csv = [
+        ['Compatibility*'] + sorted(raw.keys())
+    ]
+
+    for key in sorted(raw.keys()):
+        _line = [key]
+        for key2 in sorted(raw.keys()):
+            _line.append(raw[key][key2])
+        _csv.append(_line)
+
+    with open(_DUMP_CSV, 'w') as csvfile:
         cwriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for line in _csv[:-1]:
