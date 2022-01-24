fac7178505a30761d419dff955b50c042ec6828a
Konrad Weihmann <kweihmann@outlook.com>
2022-01-24 11:25:42 +0100

    tests: use global instead of local import
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- tests/test_compat_matrix.py
+++ tests/test_compat_matrix.py
@@ -6,33 +6,26 @@
 
 import pytest
 
-
+import osadl_matrix
 
 class TestMatrix():
-    def test_1(self):
-        import osadl_matrix
+    def test_1(self):        
         assert osadl_matrix.is_compatible("MIT", "BSD-3-Clause") == osadl_matrix.OSADLCompatibility.YES
 
     def test_2(self):
-        import osadl_matrix
         assert osadl_matrix.is_compatible("BSD-3-Clause", "MIT") ==  osadl_matrix.OSADLCompatibility.YES
 
     def test_3(self):
-        import osadl_matrix
         assert osadl_matrix.is_compatible("GPL-2.0-only", "BSD-3-Clause") ==  osadl_matrix.OSADLCompatibility.YES
 
     def test_4(self):
-        import osadl_matrix
         assert osadl_matrix.is_compatible("BSD-3-Clause", "GPL-2.0-only") ==  osadl_matrix.OSADLCompatibility.NO
 
     def test_5(self):
-        import osadl_matrix
         assert osadl_matrix.is_compatible("GPL-2.0-only", "LGPL-2.1-only") ==  osadl_matrix.OSADLCompatibility.YES
 
     def test_6(self):
-        import osadl_matrix
         assert osadl_matrix.is_compatible("GPL-2.0-only", "GPL-2.0-only") ==  osadl_matrix.OSADLCompatibility.YES
 
     def test_7(self):
-        import osadl_matrix
         assert osadl_matrix.is_compatible("My-Fairy-Little-License", "LGPL-2.1-only") ==  osadl_matrix.OSADLCompatibility.UNDEF
--- tests/test_input.py
+++ tests/test_input.py
@@ -11,14 +11,12 @@ import osadl_matrix
 class TestInput():
     def test_json(self):
         import json
-        import osadl_matrix
 
         with open(osadl_matrix.OSADL_MATRIX_JSON) as i:
             assert any(json.load(i).keys())
 
     def test_csv(self):
         import csv
-        import osadl_matrix
 
         with open(osadl_matrix.OSADL_MATRIX) as csvinput:
             creader = csv.reader(csvinput, delimiter=',', quotechar='"')
@@ -26,7 +24,6 @@ class TestInput():
 
     def test_values(self):
         import json
-        import osadl_matrix
 
         _values = set()
         with open(osadl_matrix.OSADL_MATRIX_JSON) as i:
