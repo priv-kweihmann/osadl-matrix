0621ab5d80b6676ed1486fdaab75712a264c1dff
Konrad Weihmann <kweihmann@outlook.com>
2022-01-24 11:10:55 +0100

    tests: migrate to pure pytest
    as it's way better readable and faster
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- tests/test_compat_matrix.py
+++ tests/test_compat_matrix.py
@@ -4,35 +4,35 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import os
-import sys
-import unittest
+import pytest
 
-sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 
-import osadl_matrix
 
-class TestMatrix(unittest.TestCase):
+class TestMatrix():
     def test_1(self):
-        self.assertEqual(osadl_matrix.is_compatible("MIT", "BSD-3-Clause"), osadl_matrix.OSADLCompatibility.YES)
+        import osadl_matrix
+        assert osadl_matrix.is_compatible("MIT", "BSD-3-Clause") == osadl_matrix.OSADLCompatibility.YES
 
     def test_2(self):
-        self.assertEqual(osadl_matrix.is_compatible("BSD-3-Clause", "MIT"), osadl_matrix.OSADLCompatibility.YES)
+        import osadl_matrix
+        assert osadl_matrix.is_compatible("BSD-3-Clause", "MIT") ==  osadl_matrix.OSADLCompatibility.YES
 
     def test_3(self):
-        self.assertEqual(osadl_matrix.is_compatible("GPL-2.0-only", "BSD-3-Clause"), osadl_matrix.OSADLCompatibility.YES)
+        import osadl_matrix
+        assert osadl_matrix.is_compatible("GPL-2.0-only", "BSD-3-Clause") ==  osadl_matrix.OSADLCompatibility.YES
 
     def test_4(self):
-        self.assertEqual(osadl_matrix.is_compatible("BSD-3-Clause", "GPL-2.0-only"), osadl_matrix.OSADLCompatibility.NO)
+        import osadl_matrix
+        assert osadl_matrix.is_compatible("BSD-3-Clause", "GPL-2.0-only") ==  osadl_matrix.OSADLCompatibility.NO
 
     def test_5(self):
-        self.assertEqual(osadl_matrix.is_compatible("GPL-2.0-only", "LGPL-2.1-only"), osadl_matrix.OSADLCompatibility.YES)
+        import osadl_matrix
+        assert osadl_matrix.is_compatible("GPL-2.0-only", "LGPL-2.1-only") ==  osadl_matrix.OSADLCompatibility.YES
 
     def test_6(self):
-        self.assertEqual(osadl_matrix.is_compatible("GPL-2.0-only", "GPL-2.0-only"), osadl_matrix.OSADLCompatibility.YES)
+        import osadl_matrix
+        assert osadl_matrix.is_compatible("GPL-2.0-only", "GPL-2.0-only") ==  osadl_matrix.OSADLCompatibility.YES
 
     def test_7(self):
-        self.assertEqual(osadl_matrix.is_compatible("My-Fairy-Little-License", "LGPL-2.1-only"), osadl_matrix.OSADLCompatibility.UNDEF)
-        
-if __name__ == '__main__':
-    unittest.main()
+        import osadl_matrix
+        assert osadl_matrix.is_compatible("My-Fairy-Little-License", "LGPL-2.1-only") ==  osadl_matrix.OSADLCompatibility.UNDEF
