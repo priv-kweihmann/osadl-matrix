53cec6ec2b93929ee97b0e8cea2b0c9bb0b47bff
Sandklef Henrik <hesa@sandklef.com>
2022-09-15 19:22:16 +0200

    Add function get_compatibility
    This function will provide more fine grained information about
    compatibility between two licenses

--- osadl_matrix/__init__.py
+++ osadl_matrix/__init__.py
@@ -1,19 +1,30 @@
 # SPDX-FileCopyrightText: 2021 Konrad Weihmann
+# SPDX-FileCopyrightText: 2022 Henrik Sandklef
 # SPDX-License-Identifier: Unlicensed
 
 import csv
 import os
-from enum import Enum
+from enum import Enum, unique
 
 OSADL_MATRIX = os.path.join(os.path.dirname(__file__), 'osadl-matrix.csv')
 OSADL_MATRIX_JSON = os.path.join(os.path.dirname(__file__), 'osadl-matrix.json')
 __osadl_db = {}
 
 
+@unique
 class OSADLCompatibility(Enum):
-    YES = True
-    NO = False
-    UNDEF = False
+    """
+    YES - licenses are compatible
+    NO - licenses are NOT compatible
+    UNKNOWN - license compatibility is uncertain
+    CHECKDEP - compatibility has depencies that need to be checked
+    UNDEF - at least one of the licenses are not present in the OSADL matrix
+    """
+    YES = 'Yes'
+    NO = 'No'
+    UNKNOWN = 'Unknown'
+    CHECKDEP = 'Check dependency'
+    UNDEF = 'Undefined license'
 
     @staticmethod
     def from_text(_in):
@@ -21,6 +32,10 @@ class OSADLCompatibility(Enum):
             return OSADLCompatibility.YES
         if _in == 'No':
             return OSADLCompatibility.NO
+        if _in == 'Unknown':
+            return OSADLCompatibility.UNKNOWN
+        if _in == 'Check dependency':
+            return OSADLCompatibility.CHECKDEP
         return OSADLCompatibility.UNDEF
 
 
@@ -37,14 +52,34 @@ def __read_db(customdb=None):
 
 
 def is_compatible(outbound, inbound, customdb=None):
-    """checks if the 'outbound' license is compatible with the 'inbound' license
+    """checks if the 'outbound' license is compatible with the 'inbound'
+   license. If the licenses can be found in the matrix and the
+   licenses are compatible, True is returned. In all other cases False
+   is returned.
 
     Args:
         outbound (string): SPDX ID for an outbound license, e.g. the license used for distribution of an application
         inbound (string): SPDX ID for an inbound license, e.g. the license used for a dependency of the application
 
     Returns:
-        [OSADLCompatibility]: Either yes, no or undefined
+        [OSADLCompatibility]: True or False
+
+    """
+    return get_compatibility(outbound, inbound, customdb) == OSADLCompatibility.YES
+
+
+def get_compatibility(outbound, inbound, customdb=None):
+    """returns the compatibility status between the 'outbound' and the
+    'inbound' licenses. If any (or both) of the licenses are not
+    present in the OSADL matrix, then UNDEF is returned.
+
+    Args:
+        outbound (string): SPDX ID for an outbound license, e.g. the license used for distribution of an application
+        inbound (string): SPDX ID for an inbound license, e.g. the license used for a dependency of the application
+
+    Returns:
+        [OSADLCompatibility]: Either yes, no, unknown or undefined
+
     """
     if not __osadl_db:
         __read_db(customdb=customdb)
