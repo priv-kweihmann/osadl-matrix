9fdd23dd63ec4f32853422636b60b7aef57d793f
Henrik Sandklef <hesa@sandklef.com>
2023-06-13 21:55:33 +0200

    Use OSADL's matrix directly
    Use the JSON matrix, instead of using the CSV matrix

--- osadl_matrix/__init__.py
+++ osadl_matrix/__init__.py
@@ -1,8 +1,8 @@
 # SPDX-FileCopyrightText: 2021 Konrad Weihmann
-# SPDX-FileCopyrightText: 2022 Henrik Sandklef
+# SPDX-FileCopyrightText: 2023 Henrik Sandklef
 # SPDX-License-Identifier: Unlicensed
 
-import csv
+import json
 import os
 from enum import Enum, unique
 
@@ -43,13 +43,8 @@ def __read_db(customdb=None):
     """
     global __osadl_db
     if not __osadl_db:
-        with open(customdb or OSADL_MATRIX) as i:
-            _reader = csv.DictReader(i, delimiter=',', quotechar='"')
-            for row in _reader:
-                key = row['Compatibility*']
-                for k, v in row.items():
-                    __osadl_db[(key, k)] = OSADLCompatibility.from_text(v)
-
+        with open(customdb or OSADL_MATRIX_JSON) as i:
+            __osadl_db = json.load(i)
 
 def is_compatible(outbound, inbound, customdb=None):
     """checks if the 'outbound' license is compatible with the 'inbound'
@@ -62,7 +57,7 @@ def is_compatible(outbound, inbound, customdb=None):
         inbound (string): SPDX ID for an inbound license, e.g. the license used for a dependency of the application
 
     Returns:
-        [OSADLCompatibility]: True or False
+        True or False
 
     """
     return get_compatibility(outbound, inbound, customdb) == OSADLCompatibility.YES
@@ -82,7 +77,10 @@ def get_compatibility(outbound, inbound, customdb=None):
 
     """
     __read_db(customdb=customdb)
-    return __osadl_db.get((outbound, inbound), OSADLCompatibility.UNDEF)
+    try:
+        return OSADLCompatibility.from_text(__osadl_db.get(outbound).get(inbound))
+    except:
+        return OSADLCompatibility.UNDEF
 
 
 def supported_licenses(customdb=None):
@@ -90,10 +88,4 @@ def supported_licenses(customdb=None):
     there is a known compatibility status).
     """
     __read_db(customdb=customdb)
-    licenses = set()
-    for row in __osadl_db:
-        key, k = row
-        if k == "Compatibility*":
-            continue
-        licenses.add(k)
-    return licenses
+    return __osadl_db.keys()
